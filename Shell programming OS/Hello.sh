e8d1271549e16d919d98a91d52bf993c604c7ed0
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-04-04 10:19:00 +0600

    Add files via upload

---
+++ Shell programming OS/Hello.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Enter your name"
+#reads a string
+read PERSON
+
+#prints out hello to the inputed name
+
+echo "Hello, $PERSON"
+#inputed it to keep the result visible
+
+read p
