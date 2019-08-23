48eceeaffca5dc33a4456d0a5b09f90dfa55b02b
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-08-23 15:45:34 +0600

    Add files via upload

---
+++ LinQpracticeCsharp/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace LinQpractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Login = new System.Windows.Forms.TabPage();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.setBtn = new System.Windows.Forms.Button();
+            this.passTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idTxt = new System.Windows.Forms.TextBox();
+            this.nameTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Database = new System.Windows.Forms.TabPage();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.infoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.linQpracDataSet = new LinQpractice.LinQpracDataSet();
+            this.infoTableAdapter = new LinQpractice.LinQpracDataSetTableAdapters.InfoTableAdapter();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.nameLbl = new System.Windows.Forms.Label();
+            this.idLbl = new System.Windows.Forms.Label();
+            this.fatherNameLbl = new System.Windows.Forms.Label();
+            this.motherNameLbl = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.Login.SuspendLayout();
+            this.Database.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.linQpracDataSet)).BeginInit();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Login);
+            this.tabControl1.Controls.Add(this.Database);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 25);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(776, 413);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.Login.Controls.Add(this.loginBtn);
+            this.Login.Controls.Add(this.setBtn);
+            this.Login.Controls.Add(this.passTxt);
+            this.Login.Controls.Add(this.label3);
+            this.Login.Controls.Add(this.idTxt);
+            this.Login.Controls.Add(this.nameTxt);
+            this.Login.Controls.Add(this.label2);
+            this.Login.Controls.Add(this.label1);
+            this.Login.Location = new System.Drawing.Point(4, 22);
+            this.Login.Name = "Login";
+            this.Login.Padding = new System.Windows.Forms.Padding(3);
+            this.Login.Size = new System.Drawing.Size(768, 387);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(404, 256);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 7;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // setBtn
+            // 
+            this.setBtn.Location = new System.Drawing.Point(294, 256);
+            this.setBtn.Name = "setBtn";
+            this.setBtn.Size = new System.Drawing.Size(75, 23);
+            this.setBtn.TabIndex = 6;
+            this.setBtn.Text = "Set";
+            this.setBtn.UseVisualStyleBackColor = true;
+            this.setBtn.Click += new System.EventHandler(this.setBtn_Click);
+            // 
+            // passTxt
+            // 
+            this.passTxt.Location = new System.Drawing.Point(353, 182);
+            this.passTxt.Name = "passTxt";
+            this.passTxt.Size = new System.Drawing.Size(171, 20);
+            this.passTxt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(252, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Password:";
+            // 
+            // idTxt
+            // 
+            this.idTxt.Location = new System.Drawing.Point(353, 142);
+            this.idTxt.Name = "idTxt";
+            this.idTxt.Size = new System.Drawing.Size(171, 20);
+            this.idTxt.TabIndex = 3;
+            // 
+            // nameTxt
+            // 
+            this.nameTxt.Location = new System.Drawing.Point(353, 89);
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.Size = new System.Drawing.Size(171, 20);
+            this.nameTxt.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(252, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name:";
+            // 
+            // Database
+            // 
+            this.Database.Controls.Add(this.dataGridView);
+            this.Database.Location = new System.Drawing.Point(4, 22);
+            this.Database.Name = "Database";
+            this.Database.Padding = new System.Windows.Forms.Padding(3);
+            this.Database.Size = new System.Drawing.Size(768, 387);
+            this.Database.TabIndex = 1;
+            this.Database.Text = "Database";
+            this.Database.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.idDataGridViewTextBoxColumn,
+            this.passwordDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.infoBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(205, 38);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(340, 150);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // passwordDataGridViewTextBoxColumn
+            // 
+            this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
+            this.passwordDataGridViewTextBoxColumn.HeaderText = "Password";
+            this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
+            // 
+            // infoBindingSource
+            // 
+            this.infoBindingSource.DataMember = "Info";
+            this.infoBindingSource.DataSource = this.linQpracDataSet;
+            // 
+            // linQpracDataSet
+            // 
+            this.linQpracDataSet.DataSetName = "LinQpracDataSet";
+            this.linQpracDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // infoTableAdapter
+            // 
+            this.infoTableAdapter.ClearBeforeFill = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.motherNameLbl);
+            this.tabPage1.Controls.Add(this.fatherNameLbl);
+            this.tabPage1.Controls.Add(this.idLbl);
+            this.tabPage1.Controls.Add(this.nameLbl);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(768, 387);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "ShowInfo";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(65, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 21);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(65, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 21);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "ID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(65, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 21);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "FatherName:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(65, 145);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 21);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "MotherName:";
+            // 
+            // nameLbl
+            // 
+            this.nameLbl.AutoSize = true;
+            this.nameLbl.Location = new System.Drawing.Point(196, 39);
+            this.nameLbl.Name = "nameLbl";
+            this.nameLbl.Size = new System.Drawing.Size(35, 13);
+            this.nameLbl.TabIndex = 5;
+            this.nameLbl.Text = "label8";
+            // 
+            // idLbl
+            // 
+            this.idLbl.AutoSize = true;
+            this.idLbl.Location = new System.Drawing.Point(196, 77);
+            this.idLbl.Name = "idLbl";
+            this.idLbl.Size = new System.Drawing.Size(35, 13);
+            this.idLbl.TabIndex = 6;
+            this.idLbl.Text = "label8";
+            // 
+            // fatherNameLbl
+            // 
+            this.fatherNameLbl.AutoSize = true;
+            this.fatherNameLbl.Location = new System.Drawing.Point(196, 112);
+            this.fatherNameLbl.Name = "fatherNameLbl";
+            this.fatherNameLbl.Size = new System.Drawing.Size(35, 13);
+            this.fatherNameLbl.TabIndex = 7;
+            this.fatherNameLbl.Text = "label8";
+            // 
+            // motherNameLbl
+            // 
+            this.motherNameLbl.AutoSize = true;
+            this.motherNameLbl.Location = new System.Drawing.Point(196, 151);
+            this.motherNameLbl.Name = "motherNameLbl";
+            this.motherNameLbl.Size = new System.Drawing.Size(35, 13);
+            this.motherNameLbl.TabIndex = 8;
+            this.motherNameLbl.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.Login.ResumeLayout(false);
+            this.Login.PerformLayout();
+            this.Database.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.linQpracDataSet)).EndInit();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage Database;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Button setBtn;
+        private System.Windows.Forms.TextBox passTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idTxt;
+        private System.Windows.Forms.TextBox nameTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private LinQpracDataSet linQpracDataSet;
+        private System.Windows.Forms.BindingSource infoBindingSource;
+        private LinQpracDataSetTableAdapters.InfoTableAdapter infoTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label motherNameLbl;
+        private System.Windows.Forms.Label fatherNameLbl;
+        private System.Windows.Forms.Label idLbl;
+        private System.Windows.Forms.Label nameLbl;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ LinQpracticeCsharp/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace LinQpractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'linQpracDataSet.Info' table. You can move, or remove it, as needed.
+            this.infoTableAdapter.Fill(this.linQpracDataSet.Info);
+
+        }
+
+        public void LoadInfo()
+        {
+            linqDataContext db = new linqDataContext();
+
+            var result = from c in db.Infos select c;
+            List<Info> lstInfo = result.ToList();
+            dataGridView.DataSource = lstInfo;
+        }
+
+        private void setBtn_Click(object sender, EventArgs e)
+        {
+            for_query.QueryHolder q = new for_query.QueryHolder();
+
+            q.SetQuery(nameTxt.Text, idTxt.Text, passTxt.Text);
+
+            nameTxt.Text = " ";
+            idTxt.Text = " ";
+            passTxt.Text = " ";
+
+            LoadInfo();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
