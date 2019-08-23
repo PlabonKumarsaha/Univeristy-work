9b5d6c78c2a7da6a9015e40484fb7687edaa3644
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2019-08-23 15:47:23 +0600

    Add files via upload

---
+++ GUIFrameJava/GUIFrame.java
@@ -0,0 +1,12 @@
+
+package guiframe;
+
+public class GUIFrame {
+
+    public static void main(String[] args) {
+      StudentFrame sf =new StudentFrame();
+      sf.setVisible(true);
+      
+    }
+    
+}
